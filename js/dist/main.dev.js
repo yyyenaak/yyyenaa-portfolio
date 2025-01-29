e380ee39c49cc8c0d1bc9467693ec287b701efce
yyyenaak <dpsk185@gmail.com>
2025-01-30 02:20:40 +0900

    skill 반응형 js 수정

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -4,8 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var hamburger = document.querySelector(".hamburger");
   var mobileMenu = document.querySelector(".mobile_menu");
   var cards = document.querySelectorAll(".card");
-  var titles = document.querySelectorAll(".sk_title");
-  var details = document.querySelectorAll(".sk_detail"); // 햄버거 버튼 클릭 시 메뉴 열기/닫기
+  var skillBacks = document.querySelectorAll(".skill_back"); // 햄버거 버튼 클릭 시 네비 열기/닫기
 
   hamburger.addEventListener("click", function () {
     if (mobileMenu.classList.contains("active")) {
@@ -13,13 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       openMenu();
     }
-  }); // 메뉴 열기 함수
+  }); // 네비 열기
 
   function openMenu() {
     hamburger.classList.add("active");
     mobileMenu.classList.remove("closing");
     mobileMenu.classList.add("active");
-  } // 메뉴 닫기 함수
+  } // 네비 닫기
 
 
   function closeMenu() {
@@ -36,16 +35,40 @@ document.addEventListener("DOMContentLoaded", function () {
     var isClickInside = hamburger.contains(event.target) || mobileMenu.contains(event.target);
 
     if (!isClickInside) {
-      closeMenu(); // 메뉴 닫기
+      closeMenu();
     }
   }); // 메뉴 항목 클릭 시 메뉴 닫기
 
   var menuItems = mobileMenu.querySelectorAll(".navi_font");
   menuItems.forEach(function (item) {
     item.addEventListener("click", function () {
-      closeMenu(); // 메뉴 닫기
+      closeMenu();
     });
-  }); // Intersection Observer
+  }); // skill 모바일 여닫기(클릭 시 아래로 펼쳐지게)
+
+  skillBacks.forEach(function (skillBack) {
+    skillBack.addEventListener("click", function () {
+      var detail = skillBack.querySelector(".sk_detail");
+      var isOpen = detail.classList.contains("show");
+      document.querySelectorAll(".sk_detail").forEach(function (d) {
+        if (d !== detail) {
+          d.style.maxHeight = "0px"; // 닫을 때 높이 0으로 설정
+
+          d.classList.remove("show");
+        }
+      });
+
+      if (!isOpen) {
+        detail.classList.add("show");
+        detail.style.maxHeight = detail.scrollHeight + "px";
+      } else {
+        detail.style.maxHeight = "0px";
+        setTimeout(function () {
+          detail.classList.remove("show");
+        }, 300);
+      }
+    });
+  }); // Project 스크롤 시 카드뷰 보이게
 
   var observer = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const mobileMenu = document.querySelector(".mobile_menu");
   const cards = document.querySelectorAll(".card");
-  const titles = document.querySelectorAll(".sk_title");
-  const details = document.querySelectorAll(".sk_detail");
+  const skillBacks = document.querySelectorAll(".skill_back");
 
-  // 햄버거 버튼 클릭 시 메뉴 열기/닫기
+  // 햄버거 버튼 클릭 시 네비 열기/닫기
   hamburger.addEventListener("click", () => {
     if (mobileMenu.classList.contains("active")) {
       closeMenu();
@@ -14,14 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // 메뉴 열기 함수
+  // 네비 열기
   function openMenu() {
     hamburger.classList.add("active");
     mobileMenu.classList.remove("closing");
     mobileMenu.classList.add("active");
   }
 
-  // 메뉴 닫기 함수
+  // 네비 닫기
   function closeMenu() {
     hamburger.classList.remove("active");
     mobileMenu.classList.remove("active");
@@ -36,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const isClickInside =
       hamburger.contains(event.target) || mobileMenu.contains(event.target);
     if (!isClickInside) {
-      closeMenu(); // 메뉴 닫기
+      closeMenu();
     }
   });
 
@@ -44,11 +43,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuItems = mobileMenu.querySelectorAll(".navi_font");
   menuItems.forEach((item) => {
     item.addEventListener("click", () => {
-      closeMenu(); // 메뉴 닫기
+      closeMenu();
     });
   });
 
-  // Intersection Observer
+  // skill 모바일 여닫기(클릭 시 아래로 펼쳐지게)
+  skillBacks.forEach((skillBack) => {
+    skillBack.addEventListener("click", () => {
+      const detail = skillBack.querySelector(".sk_detail");
+      const isOpen = detail.classList.contains("show");
+
+      document.querySelectorAll(".sk_detail").forEach((d) => {
+        if (d !== detail) {
+          d.style.maxHeight = "0px"; // 닫을 때 높이 0으로 설정
+          d.classList.remove("show");
+        }
+      });
+
+      if (!isOpen) {
+        detail.classList.add("show");
+        detail.style.maxHeight = detail.scrollHeight + "px";
+      } else {
+        detail.style.maxHeight = "0px";
+        setTimeout(() => {
+          detail.classList.remove("show");
+        }, 300);
+      }
+    });
+  });
+
+  // Project 스크롤 시 카드뷰 보이게
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
