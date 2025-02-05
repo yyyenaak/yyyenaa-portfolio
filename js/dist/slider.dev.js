1aaf4c1908fa14d2c376016f81c2bab2954d95b1
yyyenaak <dpsk185@gmail.com>
2025-02-06 01:41:56 +0900

    slider. fin!!

--- js/dist/slider.dev.js
+++ js/dist/slider.dev.js
@@ -7,13 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var slider = document.querySelector(".slider");
 
   function showSlide(index) {
-    // 마지막 슬라이드 이후에는 더 이상 안 넘어가도록 설정
-    if (index >= totalSlides) {
-      return;
-    } // 첫 번째 슬라이드에서 이전 버튼 클릭 시 더 이상 뒤로 가지 않도록 설정
-
-
-    if (index < 0) {
+    if (index < 0 || index >= totalSlides) {
       return;
     }
 
@@ -23,16 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function nextSlide() {
-    showSlide(currentSlide + 1);
+    if (currentSlide < totalSlides - 1) {
+      showSlide(currentSlide + 1);
+    }
   }
 
   function prevSlide() {
-    showSlide(currentSlide - 1);
-  } // 초기 슬라이드 표시
+    if (currentSlide > 0) {
+      showSlide(currentSlide - 1);
+    }
+  }
 
-
-  showSlide(currentSlide); // 버튼 이벤트 리스너 추가
-
+  showSlide(currentSlide);
   document.querySelector(".next").addEventListener("click", nextSlide);
   document.querySelector(".prev").addEventListener("click", prevSlide);
 });
